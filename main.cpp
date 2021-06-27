9c2a129cb33e77e9d51cbcbd3ce9a0d2164a819a
Axel Espinosa <axeldavid45@gmail.com>
2021-06-26 20:52:40 -0500

    feat: identify only letters

--- main.cpp
+++ main.cpp
@@ -5,14 +5,15 @@
 #include <regex>
 using namespace std;
 
-char userInput[100];
+string userInput;
 char firstLetter;
 int main () {
-    cout<<"Escribe algo:";
-    fgets(userInput, 100, stdin);
+    cout<<"Write an expression: ";
+    getline(cin, userInput);
+
 
     // Check whether the expression has white-spaces
-    for (int i = 0; i < strlen(userInput) - 1; i++) {
+    for (int i = 0; i < userInput.length(); i++) {
         if (isspace(userInput[i]) != 0) {
             fprintf(stderr, "Espacios no permitidos\n");
             exit(-1);
@@ -20,7 +21,7 @@ int main () {
     }
 
     firstLetter = userInput[0];
-    char expression[100];
+    string expression;
 
     if (isdigit(firstLetter)) {
         bool isValid = true;
@@ -28,7 +29,7 @@ int main () {
         bool foundPoint = false;
         int startingPoint = 0;
         while(isValid){
-            for (int i = startingPoint; i < strlen(userInput) - 1; i++) {
+            for (int i = startingPoint; i < userInput.length(); i++) {
                 if (isdigit(userInput[i])) {
                     expression[i] = userInput[i];
                 } else if (userInput[i] == '.') {
@@ -39,18 +40,19 @@ int main () {
                     foundPoint = true;
                     startingPoint = i + 1;
                     expressionReset = true;
-                    memset(expression,0, sizeof(expression));
+                    expression = "";
                 } else {
                     isValid = false;
                     printf("Unexpected token: %c\n", userInput[i]);
                     exit(-1);
                 }
             }
+            // Convert to false to exit from loop
             isValid = false;
 
             // Determine if the expression is an even number
             if (!expressionReset) {
-                int expressionInt = atol(expression);
+                int expressionInt = stoi(expression);
                 if ((expressionInt % 2) != 0) {
                     printf("Invalid number: %i\n", expressionInt);
                     exit(-1);
@@ -61,16 +63,17 @@ int main () {
     }
 
     if (isalpha(firstLetter)) {
-        memset(expression,0, sizeof(expression));
+        expression = "";
+        string temp = "";
         // TODO: FIND @ OR DOT(.)
         int position = 0;
         bool isAtSymbol = false;
         bool isDotSymbol = false;
 
-        regex onlyLetters("[a-z0-9_]");
+        regex validCharacter("[a-z0-9_]+");
 
         // Concat right side of the expression
-        for (int i = 0; i <= strlen(userInput) - 1; i++) {
+        for (int i = 0; i <= userInput.length() - 1; i++) {
             if (userInput[i] == '@') {
                 position = i + 1;
                 isAtSymbol = true;
@@ -80,26 +83,29 @@ int main () {
                 isDotSymbol = true;
                 break;
             } else {
-                string temp = reinterpret_cast<const char *>(userInput[i]);
+                temp += userInput[i];
                 cout<<"valor: "<<temp<< "\n";
-                if (regex_match(temp, onlyLetters)) {
-                    cout<<"valor:\n";
-                } else {
-                    cout<<"noentro\n";
+                if (!regex_match(temp, validCharacter)) {
+                    cout<<"Unexpected token "<<userInput[i];
+                    exit(-1);
                 }
             }
             position++;
         }
 
         if (isAtSymbol) {
-            // TODO
+            cout<<"is @ symbol";
         }
 
         if (isDotSymbol) {
-            // TODO
+            cout<<"is dot symbol";
         }
 
         // TODO: ONLY LETTERS
+        regex onlyLetters("[a-z]+");
+        if (regex_match(temp, onlyLetters)) {
+            cout<<"Valid expression :), only letters "<<temp;
+        }
     }
 
     if (firstLetter == '#') {
